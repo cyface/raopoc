3e2b400ab6f34df6c21fa860ea241e174a8aedc1
Tim L. White <tim@cyface.com>
2025-06-23 14:39:13 -0600

    Trying to get docker compose to work on Coolify

--- server/controllers/spa.controller.ts
+++ server/controllers/spa.controller.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 
 @Controller()
 export class SpaController {
-  @Get(['/', '/step/*', '/onboarding/*', '/login', '/register'])
+  @Get(['/', '/step/*path', '/onboarding/*path', '/login', '/register'])
   serveSpa(@Res() res: Response): void {
     // Serve the React SPA for specific routes that should be handled by React Router
     res.sendFile(join(__dirname, '..', '..', '..', 'dist', 'index.html'))
